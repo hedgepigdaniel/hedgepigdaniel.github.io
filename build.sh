82393b9d4bb61def061ad5043c4d128b080d0c76
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2020-03-23 22:14:14 +1100

    Add build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e -x
+
+BRANCH="$1"
+
+# Delete everything from the previous build
+git ls-tree --name-only -z HEAD | xargs -0 rm -rf
+
+# Check out files from the source branch
+git checkout "$BRANCH" .
+
+# Build the website
+bundle exec jekyll build
+
+git add .gitignore
+git clean -fd -e _site
+
+find _site -maxdepth 1 -mindepth 1 -exec mv {} . \;
+rm -rf _site
+
+git add .
+
+echo "Please inspect the built site, and commit/push to deploy"
